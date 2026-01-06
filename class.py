8aaf64dfe966854713c6d0fc68d2b45e281e1a3f
JCastellanosDev <jh.cas.8@gmail.com>
2026-01-06 12:23:30 -0600

    update

--- class.py
+++ class.py
@@ -5,11 +5,17 @@ class Student:
         self.enroled = enroled
         self.gpa = gpa
     def display_info(self):
-        print("The student " + self.name + "'s GPA is" + str(self.gpa))
+        print("The student " + self.name + "'s GPA is " + str(self.gpa))
+    def gratuation(self):
+        if self.enroled and self.gpa > 2.5 and self.year == 12:
+            print(self.name + " will be able to graduate this year")
+        else:
+            print(self.name + " will not be able to graduate this year")
 
-mitsuha = Student("Mitsuha", "Sophomore", True, 3.8)
-taki = Student("Taki", "Junior", True, 3.6)
+mitsuha = Student("Mitsuha", 12, True, 1.8)
+taki = Student("Taki", 12 , True, 2.6)
 
 mitsuha.display_info()
 taki.display_info()
-    
+mitsuha.gratuation()
+taki.gratuation()    
